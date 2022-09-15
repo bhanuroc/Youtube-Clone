7e19c2acb064d4cbc16cca99f1701a44d2bf0dc3
Bhanu Pratap Singh Sengar <bhanu.pratap@iitg.ac.in>
2022-09-15 19:05:45 +0530

    first commit

---
+++ src/utils/constants.js
@@ -0,0 +1,31 @@
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import HomeIcon from '@mui/icons-material/Home';
+import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+import SchoolIcon from '@mui/icons-material/School';
+import GraphicEqIcon from '@mui/icons-material/GraphicEq';
+import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
+
+export const logo = 'https://i.imgur.com/SUs02UH.png';
+
+export const categories = [
+    { name: 'New', icon: <HomeIcon />, },
+    { name: 'Music', icon: <MusicNoteIcon /> },
+    { name: 'Gaming', icon: <SportsEsportsIcon />, },
+    { name: 'Education', icon: <SchoolIcon />, },
+    { name: 'Podcast', icon: <GraphicEqIcon />, },
+    { name: 'Movie', icon: <OndemandVideoIcon />, },
+    { name: 'Sport', icon: <FitnessCenterIcon />, },
+    { name: 'Comedy', icon: <TheaterComedyIcon />, },
+    { name: 'Gym', icon: <FitnessCenterIcon />, },
+    { name: 'Crypto', icon: <DeveloperModeIcon />, },
+];
+
+export const demoThumbnailUrl = 'https://i.imgur.com/aCRUIMX.jpg';
+export const demoChannelUrl = '/channel/UCa42TtMuwceyQ0Bd6iANjmw';
+export const demoVideoUrl = '/video/GDa8kZLNhJ4';
+export const demoChannelTitle = 'Trivedi';
+export const demoVideoTitle = 'Building';
+export const demoProfilePicture = 'http://dergipark.org.tr/assets/app/images/buddy_sample.png'
